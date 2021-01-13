2d63e68601864f40328491f03b39efefd16bf131
segullshairbutt <segullshairbutt@gmail.com>
2021-01-13 21:19:02 +0500

    worker nodes and clusters added and load is generated succesfully

--- loadbalancer/monitoring_app/data_generator.py
+++ loadbalancer/monitoring_app/data_generator.py
@@ -10,24 +10,23 @@ def get_random_load(min_range, max_range):
 
 def generate_data(dir_path, config_file, data_file, **kwargs):
     cl_min_range = 1
-    cl_max_range = 90
+    cl_max_range = 100
+    cl_load = 0
 
     wn_min_range = 1
-    wn_max_range = 90
+    wn_max_range = 100
+    wn_load = 0
 
     pod_min_range = 1
     pod_max_range = 95
+    pod_load = 0
 
     container_min_range = 1
-    container_max_range = 95
-
-    method_min_range = 10
-    method_max_range = 95
-
-    cl_load = 0
-    wn_load = 0
-    pod_load = 0
+    container_max_range = 80
     container_load = 0
+
+    method_min_range = 1
+    method_max_range = 95
     method_load = 0
 
     if kwargs:
@@ -53,13 +52,16 @@ def generate_data(dir_path, config_file, data_file, **kwargs):
     # reading the latest config file
     with open(path_config, 'r', encoding='utf-8') as f:
         data = f.read()
-        template_config = json.loads(data)
+        data_template = json.loads(data)
     new_data = []
 
-    for load in range(3):
-        config = copy.deepcopy(template_config)
-        clusters = template_config["info"]["x-clusters"]
+    # set the default load value that can use only for first object in any dataset
 
+    # iterate loop 10 times to generate 10 objects of dataset
+    for load in range(3):
+        config = copy.deepcopy(data_template)
+
+        clusters = config["info"]["x-clusters"]
         for cluster in clusters.values():
             cl_load = cluster["metrics"]["load"] = get_random_load(cl_min_range, cl_max_range)
 
@@ -67,7 +69,7 @@ def generate_data(dir_path, config_file, data_file, **kwargs):
                 wn_load = wn["metrics"]["load"] = get_random_load(wn_min_range, wn_max_range)
 
                 for pod in wn["pods"].values():
-                    pod_load = pod['metrics']['load'] = get_random_load(pod_min_range, pod_max_range)
+                    pod_load = pod["metrics"]["load"] = get_random_load(pod_min_range, pod_max_range)
 
                     for container in pod['containers'].values():
                         container_load = container['metrics']['load'] = get_random_load(container_min_range,
@@ -76,25 +78,23 @@ def generate_data(dir_path, config_file, data_file, **kwargs):
         paths = config["paths"]
         for path in paths.values():
             for method in path.values():
-                method_load = method["x-metrics"]["load"] = get_random_load(method_min_range, method_max_range)
-
+                endpoint_load = get_random_load(method_min_range, method_max_range)
+                method["x-metrics"]["load"] = endpoint_load
         new_object = {
             "x-clusters": clusters,
             "paths": paths
         }
         new_data.append(new_object)
 
-        # ************************************************
-        # update the container load (min and max range)
-        if 10 < cl_load < 85:
+        if 10 < cl_load < 90:
             cl_min_range = cl_load - 10
             cl_max_range = cl_load + 10
 
-        if 10 < wn_load < 85:
+        if 10 < wn_load < 90:
             wn_min_range = wn_load - 10
             wn_max_range = wn_load + 10
 
-        if 10 < pod_load < 85:
+        if 10 < pod_load < 90:
             pod_min_range = pod_load - 10
             pod_max_range = pod_load + 10
 
@@ -102,7 +102,7 @@ def generate_data(dir_path, config_file, data_file, **kwargs):
             container_min_range = container_load - 10
             container_max_range = container_load + 10
 
-        if 10 < method_load < 85:
+        if 10 < method_load < 90:
             method_min_range = method_load - 10
             method_max_range = method_load + 10
 
